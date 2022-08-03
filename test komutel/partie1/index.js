51377d4204abf8b084bd94aaa5014c9dea7bdc76
Yahya messaoud <yahyam@greymatter.tn>
2022-08-03 11:35:16 +0100

    v2

--- test komutel/partie1/index.js
+++ test komutel/partie1/index.js
@@ -1,16 +1,14 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 app.use(express.static("public"));
 
-  
-  app.listen(4000, function () {
-    console.log("Application de test écoutant sur le port 4000 !");
-  });
+app.listen(4000, function () {
+  console.log("Application de test écoutant sur le port 4000 !");
+});
 
-  app.set('view engine', 'ejs');
-  app.set("views", "./views");
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-  
-  app.get("/userage",function(req,res){
-    res.render("Test1");
-  })
+app.get("/userage", function (req, res) {
+  res.render("Test1");
+});
--- test komutel/partie2/index.js
+++ test komutel/partie2/index.js
@@ -1,75 +1,85 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 app.use(express.static("public"));
 
-  
+// Dans cette méthode on fait le calcul du temps nécessaire pour depasser le premier feu si son etat est rouge.
 
+function calculTempsPremierFeu(DelayPremierFeu, DelayAvantFeu) {
+  return (
+    2 * DelayAvantFeu + (DelayPremierFeu - (DelayAvantFeu % DelayPremierFeu))
+  );
+}
 
-const feux = [10, 11, 12, 13, 14, 15]
-let vitesse = 5 ;
+// Dans cette méthode on distingue l'etat du feu s'il est rouge ou bien vert
 
+function testEtatFeu(SommeDuTemps, feu) {
+  return Math.trunc(SommeDuTemps / feu) % 2;
+}
 
-function verification(feux , vitesse){
+// Dans cette méthode on fait le calcul du temps nécessaire pour dépasser les autres feux aprés le premier si son etat est rouge.
 
-  let sommeSeconde = 0 ;
-  let HorsFeuDebut = 150/vitesse;
+function calculTempsFeu(SommeDuTemps, feu, DelayAvantFeu) {
+  return SommeDuTemps + (feu - (SommeDuTemps % feu)) + DelayAvantFeu;
+}
 
+// Dans cette méthode on fait le calcul global du temps de réponse en utilisant les méthodes définies ci-dessus.
+
+function verificationGLobal(feux, vitesse) {
+  // Une variable qui definie le temps necessaire globalement pour depasser tous les feux.
+  let sommeSeconde = 0;
+
+  // Une variable qui fait le calcul du temps depasser hors des l'intervalle des feux.
+  let HorsFeuDebut = 150 / vitesse;
 
   if (HorsFeuDebut >= feux[0]) {
-    sommeSeconde = (2*HorsFeuDebut + (feux[0] - (HorsFeuDebut%10))) ;
-    console.log( "feu 0 = " + sommeSeconde)
-}
-else {sommeSeconde = ( 2*HorsFeuDebut) ;
-      console.log( "feu 0 = " + sommeSeconde);}  
+    sommeSeconde = calculTempsPremierFeu(feux[0], HorsFeuDebut);
+  } else {
+    sommeSeconde = 2 * HorsFeuDebut;
+  }
 
-for (let index = 1; index < feux.length; index++) {
+  for (let index = 1; index < feux.length; index++) {
+    if (testEtatFeu(sommeSeconde, feux[index])) {
+      sommeSeconde = calculTempsFeu(sommeSeconde, feux[index], HorsFeuDebut);
+    } else {
+      sommeSeconde = sommeSeconde + HorsFeuDebut;
+    }
+  }
 
-if ((Math.trunc(sommeSeconde/(feux[index]))%2)!=0) { 
-sommeSeconde = (sommeSeconde + (feux[index] - (sommeSeconde%10)) + HorsFeuDebut) ;
-console.log( "feu "+index+" = " + sommeSeconde)
-
-}
-else {sommeSeconde = (sommeSeconde + HorsFeuDebut) ;
-console.log( "feu "+index+" = " + sommeSeconde)}  
-
+  console.log("\n=> La durée Finale = " + sommeSeconde);
 }
 
-console.log("Durée Finale = "+ (sommeSeconde))
+app.listen(4000, function () {
+  console.log("\nExemple 1 --  Feux : [10, 10, 10] , Vitesse :  30");
 
+  verificationGLobal([10, 10, 10], 30);
 
-}
+  console.log("\nExemple 2 -- Feux : [10, 10, 10] , Vitesse :  20");
 
+  verificationGLobal([10, 10, 10], 20);
 
+  console.log("\nExemple 3 -- Feux : [10, 20, 30] , Vitesse :  20");
 
+  verificationGLobal([10, 20, 30], 20);
 
+  console.log("\nExemple 4 -- Feux : [10, 11, 12, 13, 14, 15] , Vitesse :  5");
 
+  verificationGLobal([10, 11, 12, 13, 14, 15], 5);
 
+  console.log(
+    "\nExemple 5 -- Feux : [60, 60, 60, 60, 60, 60, 60, 60, 60, 60] , Vitesse :  5"
+  );
 
+  verificationGLobal([60, 60, 60, 60, 60, 60, 60, 60, 60, 60], 5);
 
+  console.log(
+    "\nExemple 5 -- Feux : [55, 29, 26, 12, 19, 39, 18, 20, 23, 28, 56, 20, 59, 48,33, 40, 30, 60, 19] , Vitesse :  25"
+  );
 
-
-
-
-  app.listen(4000, function () {
-
-console.log("************* Feux : [10, 10, 10] , Vitesse :  30 ***********")
-
-    verification([10, 10, 10] , 30) ;
-
-console.log("************* Feux : [10, 10, 10] , Vitesse :  20 ***********")
-
-    verification([10, 10, 10] , 20) ;
-
-
-console.log("************* Feux : [10, 20, 30] , Vitesse :  20 ***********")
-
-    verification([10, 20, 30] , 20) ;
-
- console.log("************* Feux : [60, 60, 60, 60, 60, 60, 60, 60, 60, 60] , Vitesse :  5 ***********")
-
-    verification([60, 60, 60, 60, 60, 60, 60, 60, 60, 60] , 5) ;
-  });
-
-
-
-  
+  verificationGLobal(
+    [
+      55, 29, 26, 12, 19, 39, 18, 20, 23, 28, 56, 20, 59, 48, 33, 40, 30, 60,
+      19,
+    ],
+    25
+  );
+});
